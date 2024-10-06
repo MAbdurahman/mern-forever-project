ce9b49579050c847a74d78f73b08333ed37f9324
MAbdurahman <mdbdrrhm@gmail.com>
2024-10-06 04:02:07 -0500

    Add basic structure for context/shopContext.js

---
+++ frontend/src/context/shopContext.js
@@ -0,0 +1,28 @@
+import {createContext, useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import axios from 'axios';
+import { products } from '../assets/img/assets.js';
+
+export const ShopContext = createContext();
+
+
+const ShopContextProvider = ({props}) => {
+
+   const currency = '$';
+   const deliveryFee = 10;
+
+
+   const value = {
+      products: products,
+      currency: currency,
+      deliveryFee: deliveryFee,
+   }
+   return (
+      <ShopContext.Provider value={value}>
+         {props.children}
+      </ShopContext.Provider>
+   );
+
+}
+export default ShopContextProvider;

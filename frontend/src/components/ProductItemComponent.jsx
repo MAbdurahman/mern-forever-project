bb20fbaf283f50e845aee674c73d4ab29b3056ae
MAbdurahman <mdbdrrhm@gmail.com>
2024-10-06 05:57:27 -0500

    Update img tag, alt attribute

--- frontend/src/components/ProductItemComponent.jsx
+++ frontend/src/components/ProductItemComponent.jsx
@@ -8,7 +8,7 @@ export default function ProductItemComponent({id,image,name,price}) {
    return (
       <Link onClick={()=>scrollTo(0,0)} className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
          <div className=' overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="single product image" />
          </div>
          <p className='pt-3 pb-1 text-sm'>{name}</p>
          <p className=' text-sm font-medium'>{currency}{price}</p>

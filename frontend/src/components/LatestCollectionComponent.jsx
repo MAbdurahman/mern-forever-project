8fb455b3fb6867c6070cc0eb431a93140c391bc1
MAbdurahman <mdbdrrhm@gmail.com>
2024-10-06 05:34:06 -0500

    Add TitleComponent to components/LatestCollectionComponent.jsx

--- frontend/src/components/LatestCollectionComponent.jsx
+++ frontend/src/components/LatestCollectionComponent.jsx
@@ -1,15 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext.jsx';
+import TitleComponent from './TitleComponent.jsx';
 
 export default function LatestCollectionComponent() {
    const { products } = useContext(ShopContext);
    const [latestProducts,setLatestProducts] = useState([]);
 
-   console.log(products);
+
 
    return (
-      <div>
-         <h2>LatestCollectionComponent</h2>
+      <div className='my-10'>
+         <div className='text-center py-8 text-3xl'>
+            <TitleComponent text1={'LATEST'} text2={'COLLECTIONS'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+               Lorem Ipsum is simply dummy text of the printing and typesetting
+               industry. Lorem Ipsum has been the.
+            </p>
+         </div>
+
+         {/* Rendering Products */}
+         {/*<div
+            className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+               latestProducts.map((item, index) => (
+                  <ProductItem key={index} id={item._id} image={item.image}
+                               name={item.name} price={item.price}/>
+               ))
+            }
+         </div>*/}
       </div>
 
    );

d62a88447d6135cf55832ada60dd9c53a884aab0
MAbdurahman <mdbdrrhm@gmail.com>
2024-10-07 15:45:31 -0500

    Add components/RelatedProductsComponent.jsx

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -9,7 +9,7 @@ export default function FooterComponent() {
 
             <div>
                <img src={assets.logo} className='mb-5 w-32' alt=""/>
-               <p className='w-full md:w-2/3 text-gray-600'>
+               <p className='w-full tracking-wider md:w-2/3 text-gray-600'>
                   Lorem Ipsum is simply dummy text of the printing and typesetting
                   industry. Lorem Ipsum has been the industry's standard dummy text
                   ever since the 1500s, when an unknown printer took a galley of type
@@ -18,7 +18,7 @@ export default function FooterComponent() {
             </div>
 
             <div>
-               <p className='text-xl font-medium mb-5'>COMPANY</p>
+               <p className='text-xl tracking-wider font-medium mb-5'>COMPANY</p>
                <ul className='flex flex-col gap-1 text-gray-600'>
                   <li>Home</li>
                   <li>About us</li>
@@ -28,8 +28,8 @@ export default function FooterComponent() {
             </div>
 
             <div>
-               <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-               <ul className='flex flex-col gap-1 text-gray-600'>
+               <p className='text-xl tracking-wider font-medium mb-5'>GET IN TOUCH</p>
+               <ul className='flex flex-col tracking-wider gap-1 text-gray-600'>
                   <li>+1-212-456-7890</li>
                   <li>contact@foreveryou.com</li>
                </ul>
--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -12,19 +12,19 @@ export default function NavbarComponent() {
          <Link to='/'><img src={assets.logo} className='w-36'
                            alt="brand logo"/></Link>
          <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-            <NavLink to='/' className='flex flex-col items-center gap-1'>
+            <NavLink to='/' className='flex flex-col tracking-wider items-center gap-1'>
                <p>HOME</p>
                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
-            <NavLink to='/about' className='flex flex-col items-center gap-1'>
+            <NavLink to='/about' className='flex flex-col tracking-wider items-center gap-1'>
                <p>ABOUT</p>
                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
-            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+            <NavLink to='/collection' className='flex flex-col tracking-wider items-center gap-1'>
                <p>COLLECTION</p>
                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
-            <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+            <NavLink to='/contact' className='flex flex-col tracking-wider items-center gap-1'>
                <p>CONTACT</p>
                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
@@ -36,9 +36,9 @@ export default function NavbarComponent() {
                <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="profile icon"/>
                {/* Dropdown Menu */}
                <div
-                  className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                  className='group-hover:block hidden absolute tracking-wider dropdown-menu right-0 pt-4'>
                   <div
-                     className='flex flex-col gap-2 w-36 py-3 px-5  bg-slate-100 text-gray-500 rounded'>
+                     className='flex flex-col gap-2 w-36 py-3 px-5 tracking-wider bg-slate-100 text-gray-500 rounded'>
                      <p className='cursor-pointer hover:text-black'>My Profile</p>
                      <p className='cursor-pointer hover:text-black'>Orders</p>
                      <p className='cursor-pointer hover:text-black'>Sign Out</p>

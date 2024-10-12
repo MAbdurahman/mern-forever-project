f6105855ffd4757fc95f8c8b5539fdfb8cd69c5e
MAbdurahman <mdbdrrhm@gmail.com>
2024-10-12 02:20:37 -0500

    Add BrowserRoute to main.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,9 @@
+import React from 'react';
 export default function App() {
 
    return (
       <div>
-         <h2 className="text-3xl font-bold underline">
+         <h2 className="text-3xl font-bold">
             Admin App
          </h2>
       </div>
